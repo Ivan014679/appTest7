e04f706857268e35307856162a1657311ee0adaa
Ivan014679 <ivannarvaez774@gmail.com>
2019-09-18 08:42:06 -0500

    Add Colors project

---
+++ MyApps/myColorsApp/app/src/main/java/com/example/mycolorsapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mycolorsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    //1. Instance id's from xml form design
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View vieColors = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //2. Get id's from xml form design
+        sbrRed = findViewById(R.id.sbrRed);
+        sbrGreen = findViewById(R.id.sbrGreen);
+        sbrBlue = findViewById(R.id.sbrBlue);
+        sbrAlpha = findViewById(R.id.sbrAlpha);
+        vieColors = findViewById(R.id.vieColors);
+
+        //3. Set Seekbar change on moving
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+
+        //4. Touch on vieColors. Enable context menu on view component
+        registerForContextMenu(vieColors);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int b = sbrBlue.getProgress();
+        int a = sbrAlpha.getProgress();
+
+        int color = Color.argb(a, r, g, b);
+        vieColors.setBackgroundColor(color);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    /*Tarea
+    1. Aplicar el menu contextual
+    2. Aplicar el menu de opciones
+    3. Reset icon = Default (0)
+    4. Crear el acerca de (nueva actividad que contenga la información personal)
+     */
+}
