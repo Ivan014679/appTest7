ec345271be163a1d9ec16667b66b0184e1d1128a
Ivan014679 <ivannarvaez774@gmail.com>
2019-10-16 08:44:52 -0500

    Uploading proyect pets with many improvements

--- MyApps/Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ MyApps/Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -1,16 +1,13 @@
 package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.pets.database.PetsDB;
@@ -19,7 +16,6 @@ import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText email, password;
-    private ImageView eyepassword;
     private String regex_email = "[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}";
 
     @Override
@@ -29,47 +25,6 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.editTextE);
         password = findViewById(R.id.editTextP);
-        eyepassword = findViewById(R.id.imageEyeP);
-
-        eyepassword.setVisibility(View.GONE);
-
-        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-
-        password.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(password.getText().length() > 0){
-                    eyepassword.setVisibility(View.VISIBLE);
-                }else{
-                    eyepassword.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        eyepassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(eyepassword.getBackground() == ResourcesCompat.getDrawable(getResources(), R.drawable.ic_eye_show, null)){
-                    eyepassword.setBackgroundResource(R.drawable.ic_eye_hide);
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    password.setSelection(password.length());
-                }else{
-                    eyepassword.setBackgroundResource(R.drawable.ic_eye_show);
-                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    password.setSelection(password.length());
-                }
-            }
-        });
     }
 
     public void signIn(View view){
@@ -79,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
 
         if(!onlinemail.isEmpty() && !pass.isEmpty()) {
             Pattern patron = Pattern.compile(regex_email);
-            if(patron.matcher(onlinemail.trim()).matches()){
+            if(!Patterns.EMAIL_ADDRESS.matcher(onlinemail.trim()).matches()){
                 String[] user = {onlinemail, pass};
                 PetsDB petsDB = new PetsDB(this,
                         "pets", null, 1);
--- MyApps/Pets/app/src/main/java/com/example/pets/RegisterActivity.java
+++ MyApps/Pets/app/src/main/java/com/example/pets/RegisterActivity.java
@@ -1,16 +1,12 @@
 package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.pets.database.PetsDB;
@@ -18,9 +14,8 @@ import com.example.pets.database.PetsDB;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText fname, lname, username, email, password, cpassword;
-    private ImageView eyepassword, eyecpassword;
-    private String regex_email = "[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}";
+    private EditText fname, lname, email, password, cpassword;
+    private String regex_pass = "^[a-zA-Z0-9]+$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,86 +27,6 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.editTextEmail);
         password = findViewById(R.id.editTextPassword);
         cpassword = findViewById(R.id.editTextCPassword);
-        eyepassword = findViewById(R.id.imageEyePassword);
-        eyecpassword = findViewById(R.id.imageEyeCPassword);
-
-        eyepassword.setVisibility(View.GONE);
-        eyecpassword.setVisibility(View.GONE);
-
-        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        cpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-
-        password.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(password.getText().length() > 0){
-                    eyepassword.setVisibility(View.VISIBLE);
-                }else{
-                    eyepassword.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        cpassword.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(cpassword.getText().length() > 0){
-                    eyecpassword.setVisibility(View.VISIBLE);
-                }else{
-                    eyecpassword.setVisibility(View.GONE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        eyepassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(eyepassword.getBackground() == ResourcesCompat.getDrawable(getResources(), R.drawable.ic_eye_show, null)){
-                    eyepassword.setBackgroundResource(R.drawable.ic_eye_hide);
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    password.setSelection(password.length());
-                }else{
-                    eyepassword.setBackgroundResource(R.drawable.ic_eye_show);
-                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    password.setSelection(password.length());
-                }
-            }
-        });
-
-        eyecpassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(eyecpassword.getBackground() == ResourcesCompat.getDrawable(getResources(), R.drawable.ic_eye_show, null)){
-                    eyecpassword.setBackgroundResource(R.drawable.ic_eye_hide);
-                    cpassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    cpassword.setSelection(cpassword.length());
-                }else{
-                    eyecpassword.setBackgroundResource(R.drawable.ic_eye_show);
-                    cpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    cpassword.setSelection(cpassword.length());
-                }
-            }
-        });
     }
 
     public void signUp(View view) {
@@ -123,10 +38,12 @@ public class RegisterActivity extends AppCompatActivity {
         String cpass = cpassword.getText().toString();
 
         if(!firstname.isEmpty() && !lastname.isEmpty() && !onlinemail.isEmpty() && !pass.isEmpty() && !cpass.isEmpty()) {
-            Pattern patron = Pattern.compile(regex_email);
-            if(!patron.matcher(onlinemail.trim()).matches()) {
+            Pattern patron = Pattern.compile(regex_pass);
+            if(!Patterns.EMAIL_ADDRESS.matcher(onlinemail.trim()).matches()) {
                 Toast.makeText(this, getString(R.string.incorrectemail), Toast.LENGTH_SHORT).show();
-            }else if(pass != cpass){
+            }else if(!patron.matcher(pass).matches() || pass.length() < 4){
+                Toast.makeText(this, getString(R.string.wrongpassword), Toast.LENGTH_SHORT).show();
+            }else if(!pass.contentEquals(cpass)){
                 Toast.makeText(this, getString(R.string.toastpassconfirm), Toast.LENGTH_SHORT).show();
             }else{
                 String[] user = {firstname, lastname, onlinemail, pass};
